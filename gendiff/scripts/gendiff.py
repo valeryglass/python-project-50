684a0d74b7a1872728b210cbce62e241f9e603c3
Your Name <mesto3@gmailcom>
2023-06-21 21:48:07 +0200

    style: Edit lint errs

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,22 +6,21 @@ import json
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.', usage='%(prog)s [-h] [-f FORMAT] first_file second_file')
+    parser = argparse.ArgumentParser(
+            description='Compares two configuration files'
+            ' and shows a difference.',
+            usage='%(prog)s [-h] [-f FORMAT] first_file second_file'
+    )
     parser.add_argument('first_file', help='First configuration file')
     parser.add_argument('second_file', help='Second configuration file')
     parser.add_argument('-f', '--format', help='set format of output')
-
-
     # Parse the command-line arguments
     args = parser.parse_args()
-
-
     # Access the values passed for file_path1 and file_path2
     file_path1 = args.first_file
     file_path2 = args.second_file
-    format_option = args.format
-   
-    # Perform operations 
+    # format_option = args.format
+    # Perform operations
     diff = generate_diff(file_path1, file_path2)
     print(diff)
 
@@ -50,8 +49,11 @@ def generate_diff(file_path1, file_path2):
 # Sort keys in 'diff' by the third symbol
     sorted_diff = dict(sorted(diff.items(), key=lambda x: x[0][2]))
 # Make string from 'diff'
-    result = "\n".join([f"{key}: {value}" for key, value in sorted_diff.items()])
+    result = "\n".join(
+            [f"{key}: {value}" for key, value in sorted_diff.items()]
+    )
     return f"{{\n{result}\n}}"
-   
+
+
 if __name__ == '__main__':
     main()
