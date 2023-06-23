9ece9266e55e0e122d5504b1eda7ea4b0866dfbf
Your Name <mesto3@gmailcom>
2023-06-23 11:09:52 +0200

    refactor: Impliment less cog complex logic

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -25,7 +25,8 @@ def main():
     print(diff)
 
 
-# Diff maker logic
+
+
 def generate_diff(file_path1, file_path2):
     '''
     Compare two JSON files and return a string with the differences
@@ -33,26 +34,39 @@ def generate_diff(file_path1, file_path2):
     with open(file_path1) as file1, open(file_path2) as file2:
         data1 = json.load(file1)
         data2 = json.load(file2)
-# Build the 'diff' dictionary
+
+    diff = compare_data(data1, data2)
+    sorted_diff = sort_diff_keys(diff)
+    result = format_diff(sorted_diff)
+
+    return f"{{\n{result}\n}}"
+
+
+def compare_data(data1, data2):
     diff = {}
+
     for key in data1:
         if key not in data2:
             diff['- ' + key] = data1[key]
         elif data1[key] != data2[key]:
             diff['- ' + key] = data1[key]
             diff['+ ' + key] = data2[key]
-        elif data1[key] == data2[key]:
+        else:
             diff['  ' + key] = data1[key]
+
     for key in data2:
         if key not in data1:
             diff['+ ' + key] = data2[key]
-# Sort keys in 'diff' by the third symbol
-    sorted_diff = dict(sorted(diff.items(), key=lambda x: x[0][2]))
-# Make string from 'diff'
-    result = "\n".join(
-            [f"{key}: {value}" for key, value in sorted_diff.items()]
-    )
-    return f"{{\n{result}\n}}"
+
+    return diff
+
+
+def sort_diff_keys(diff):
+    return dict(sorted(diff.items(), key=lambda x: x[0][2]))
+
+
+def format_diff(sorted_diff):
+    return "\n".join([f"{key}: {value}" for key, value in sorted_diff.items()])
 
 
 if __name__ == '__main__':
