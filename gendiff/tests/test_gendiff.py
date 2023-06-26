8c7b4b4daea22f6c9d71ba2e04eb88248d55234c
Your Name <mesto3@gmailcom>
2023-06-26 22:20:22 +0200

    test: Add fixtures and edit test_gendiff

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,26 +1,30 @@
 # test_gendiff.py
 
 import pytest
+import os
 from gendiff.scripts.gendiff import generate_diff
 
 
 @pytest.fixture
 def file_path1():
-    return 'file1.json'
+    return os.path.join("gendiff", "tests", "fixtures", "file1.json")
 
 
 @pytest.fixture
 def file_path2():
-    return 'file2.json'
+    return os.path.join("gendiff", "tests", "fixtures", "file2.json")
 
 
-def test_generate_diff(file_path1, file_path2):
-    expected_diff = """{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
-}"""
-    assert generate_diff(file_path1, file_path2) == expected_diff
+@pytest.fixture
+def expected_diff():
+    with open(os.path.join(
+                           "gendiff",
+                           "tests",
+                           "fixtures",
+                           "expected_diff.txt"
+    )) as f:
+        return f.read()
+
+
+def test_generate_diff(file_path1, file_path2, expected_diff):
+    assert generate_diff(file_path1, file_path2) == expected_diff.strip()
